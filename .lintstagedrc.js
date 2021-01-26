c03c22ceba93115ed38f3c48f1f3b8a8d2e51ce8
Bryan Hoang <bryan.c.hoang@gmail.com>
2021-01-25 21:38:34 -0500

    chore(.eslintignore): make eslint lint js dotfiles

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,13 +4,13 @@ const prettierCommand = 'npm run format -- --write';
 const markdownlintCommand = 'npm run markdownlint -- --fix';
 
 const typescriptFiles = '*.@(ts|tsx)';
-const eslintableFiles = '*.@(ts|tsx|js)';
+const eslintFiles = '*.@(ts|tsx|js)';
 const markdownFiles = '*.@(md)';
 const prettierableFiles = '*.@(md|json|svg|yaml|yml)';
 
 module.exports = {
   [typescriptFiles]: [typeCheckCommand],
-  [eslintableFiles]: [eslintCommand],
+  [eslintFiles]: [eslintCommand],
   [markdownFiles]: [markdownlintCommand],
   [prettierableFiles]: [prettierCommand],
 };
