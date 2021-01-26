c92e669792398d833bc0edec5343d95f98b579b0
Bryan Hoang <bryan.c.hoang@gmail.com>
2021-01-25 20:50:44 -0500

    chore(.lintstagedrs.js): add .yml files to lint-staged detection

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,7 +6,7 @@ const markdownlintCommand = 'npm run markdownlint -- --fix';
 const typescriptFiles = '*.@(ts|tsx)';
 const eslintableFiles = '*.@(ts|tsx|js)';
 const markdownFiles = '*.@(md)';
-const prettierableFiles = '*.@(md|json|svg|yaml)';
+const prettierableFiles = '*.@(md|json|svg|yaml|yml)';
 
 module.exports = {
   [typescriptFiles]: [typeCheckCommand],
